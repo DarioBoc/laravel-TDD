f90566aadd76a1bda184fd22a26df8d06b991b70
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-04-27 22:18:11 -0300

    Update route service provider

---
+++ routes/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+// Posts
+Route::get('posts/create', [
+    'uses' => 'CreatePostController@create',
+    'as' => 'posts.create'
+]);
+
+Route::post('posts/store', [
+    'uses' => 'CreatePostController@store',
+    'as' => 'posts.store'
+]);
