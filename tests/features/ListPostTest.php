2477538428d5554c3695aedfdd90c04980a6bfd8
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-04-29 20:51:13 -0300

    Add posts list pagination test

--- tests/features/ListPostTest.php
+++ tests/features/ListPostTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 class ListPostTest extends FeatureTestCase
 {
     public function test_a_user_can_see_the_posts_list_and_go_to_the_details()
@@ -13,5 +15,29 @@ class ListPostTest extends FeatureTestCase
             ->see($post->title)
             ->click($post->title)
             ->seePageIs($post->url);
+    }
+
+    public function test_the_posts_are_paginated()
+    {
+        $first = $this->createPost([
+            'title' => 'old post',
+            'created_at' => Carbon::now()->subDays(2)
+        ]);
+
+        factory(\App\Post::class)->times(15)->create([
+            'created_at' => Carbon::now()->subDay()
+        ]);
+
+        $last = $this->createPost([
+            'title' => 'recent post',
+            'created_at' => Carbon::now()
+        ]);
+
+        $this->visit('/')
+            ->see($last->title)
+            ->dontSee($first->title)
+            ->click('2')
+            ->see($first->title)
+            ->dontSee($last->title);
     }
 }
