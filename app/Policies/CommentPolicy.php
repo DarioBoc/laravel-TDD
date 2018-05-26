689b64e14429acf90c8b6f119a1611686e2da436
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-05-25 21:51:51 -0300

    Add comment policy to accept a comment as answer

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Comment;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CommentPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function accept(User $user, Comment $comment)
+    {
+        return $user->owns($comment->post);
+    }
+}
